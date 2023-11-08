e7fd735b20ebeac2e12575e0aa1ff74d214d581e
Gur <guermoud.@gmail.com>
2023-11-08 22:03:17 +0100

    Added Employe and Ouvrier classes

---
+++ ClassesAbstraites/TP5_ClasseAbstraites/Employe.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TP5_ClasseAbstraites
+{
+    abstract internal class Employe
+    {
+        protected static int matr;
+        protected int nbr = 0;
+        protected string nom;
+        protected string prenom;
+        protected DateTime d;
+        public Employe(string nom, string prenom,DateTime d)
+        {
+            nbr++;
+            matr = nbr;
+            this.nom = nom;
+            this.prenom = prenom;
+            this.d = d;
+        }
+        public int Matr
+        {
+            get { return matr; }
+        }
+        public string Nom
+        {
+            get { return nom; }
+            set { nom = value; }
+        }
+        public string Prenom
+        {
+            get { return prenom; }
+            set { prenom =  value; }
+        }
+        public  DateTime D
+        {
+            get { return d; }
+            set { d = value; }
+        }
+        public override string ToString()
+        {
+            return "[matr: " + matr + ", nom: " + nom + ", prenom: " + prenom + ", dateNaissance: " + d + "]";
+
+        }
+        public abstract double CalculerSalaire();
+
+
+
+
+
+    }
+}
---
+++ ClassesAbstraites/TP5_ClasseAbstraites/Ouvrier.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TP5_ClasseAbstraites
+{
+    internal class Ouvrier : Employe 
+    {
+        private DateTime dateEntree;
+        private int smig = 2500;
+        private double salaireMensuel;
+        public Ouvrier(DateTime dateEntree, string nom, string prenom) : base(nom, prenom, dateEntree)
+        {
+            this.dateEntree = dateEntree;
+        }
+        public override double CalculerSalaire()
+        {
+
+            salaireMensuel = smig + (DateTime.Now.Year - dateEntree.Year)*100;
+            if(salaireMensuel < smig*2)
+            {
+                return salaireMensuel;
+            }
+            return -1; 
+        }
+    }
+}
